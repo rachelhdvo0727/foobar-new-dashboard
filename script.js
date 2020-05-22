5bd18759d2626c805c49065673c72eb7fc5e1102
jenniferjaque <jenn3204@stud.kea.dk>
2020-05-22 20:06:33 +0200

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 "use strict";
-window.addEventListener("DOMContentLoaded", start);
-import "@babel/polyfill";
-import moment from "moment";
 import { heroku } from "./components/js/heroku";
+
+window.addEventListener("DOMContentLoaded", start);
+// import "@babel/polyfill";
+// import moment from "moment";
 
 function start() {
   console.log("start");
-  heroku.getData();
+  heroku.getData(showData);
   //heroku.postOrder();
+}
+
+function showData(data) {
+  console.log(data);
+  console.log(data.storage[0].amount);
+  console.log(data.storage[1].amount);
+  console.log(data.storage[2].amount);
+  console.log(data.storage[3].amount);
+  console.log(data.storage[4].amount);
+  console.log(data.storage[5].amount);
 }
