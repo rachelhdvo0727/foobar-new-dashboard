fa141bade3096431fed09b2770fda85dcc3243e1
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-05-23 18:09:49 +0200

    script added for order numbers

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ window.addEventListener("DOMContentLoaded", start);
 import "@babel/polyfill";
 import moment from "moment";
 import { heroku } from "./components/js/heroku";
+import { foobarUrl, beertypesUrl, orderUrl } from "./components/js/vars";
 
 function start() {
   console.log("start");
+  getData();
   heroku.getData();
-  //heroku.postOrder();
+}
+
+function getData() {
+  fetch(foobarUrl, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  })
+    .then((res) => res.json())
+    .then(getOrderNo);
+}
+
+function getOrderNo(order) {
+  document.querySelector(".order-number").textContent = order.queue.length;
+  setInterval(getData, 10000);
 }
