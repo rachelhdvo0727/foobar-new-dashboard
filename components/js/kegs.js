1467b63636234c3c3f9ecb08127ced2f20bfc401
jenniferjaque <jenn3204@stud.kea.dk>
2020-05-24 23:53:06 +0200

    data working, styling/responsive NOT working

--- components/js/kegs.js
+++ components/js/kegs.js
@@ -3,10 +3,13 @@ const regeneratorRuntime = require("regenerator-runtime");
 
 export function kegsStart() {
   heroku.getData(dataForKegs);
+  setInterval(() => {
+    document.querySelector("#kegs_container").innerHTML = "";
+    heroku.getData(dataForKegs);
+  }, 10000);
 
   function dataForKegs(data) {
     console.log(data);
-    console.log(data.storage[0].amount);
 
     getSvg("svg/only-keg.svg", svgForKegs, data);
   }
@@ -18,39 +21,25 @@ export function kegsStart() {
   }
 
   function svgForKegs(svg, data) {
-    console.log("hej" + svg);
-    console.log(data.storage[0].amount);
+    let taps = data.taps;
+    taps.forEach((tap) => {
+      let storage = data.storage;
+      let filter = storage.filter(function (storage) {
+        return storage.name == tap.beer;
+      });
 
-    createKeg(1, svg, "beers_images/elhefe.png", data.storage[0].amount);
-    createKeg(2, svg, "beers_images/fairytaleale.png", data.storage[1].amount);
-    createKeg(3, svg, "beers_images/githop.png", data.storage[2].amount);
-    createKeg(4, svg, "beers_images/hollaback.png", data.storage[3].amount);
-    createKeg(
-      5,
-      svg,
-      "beers_images/hoppilyeverafter.png",
-      data.storage[4].amount
-    );
-    createKeg(6, svg, "beers_images/mowintime.png", data.storage[5].amount);
-    createKeg(7, svg, "beers_images/row26.png", data.storage[6].amount);
-    createKeg(
-      8,
-      svg,
-      "beers_images/ruinedchildhood.png",
-      data.storage[7].amount
-    );
-    createKeg(9, svg, "beers_images/sleighride.png", data.storage[8].amount);
-    createKeg(10, svg, "beers_images/steampunk.png", data.storage[9].amount);
+      createKeg(tap.id, svg, tap.beer, filter[0].amount, tap.level);
+    });
   }
 
-  function createKeg(number, svg, image, storage) {
+  function createKeg(number, svg, beertype, storage, level) {
     let kegContainer = document.createElement("article");
     kegContainer.classList.add("keg");
     document.querySelector("#kegs_container").appendChild(kegContainer);
 
     let kegNumber = document.createElement("div");
     kegNumber.classList.add("keg_number");
-    kegNumber.textContent = number;
+    kegNumber.textContent = number + 1;
     kegContainer.appendChild(kegNumber);
     console.log(kegNumber);
 
@@ -61,8 +50,19 @@ export function kegsStart() {
     kegContainer.appendChild(kegSVG);
 
     let kegIMG = document.createElement("img");
-    kegIMG.src = image;
-    kegIMG.alt = "beer";
+    let beerLower = beertype.toLowerCase();
+    let beerArray = beerLower.split(" ");
+    console.log(beerArray.length);
+    if (beerArray.length < 2) {
+      console.log("hallo!!" + beerArray[0] + ".png");
+      kegIMG.src = "beers_images/" + beerArray[0] + ".png";
+    } else if (beerArray.length == 2) {
+      kegIMG.src = "beers_images/" + beerArray[0] + beerArray[1] + ".png";
+    } else if (beerArray.length == 3) {
+      kegIMG.src =
+        "beers_images/" + beerArray[0] + beerArray[1] + beerArray[2] + ".png";
+    }
+    kegIMG.alt = beertype;
     kegIMG.classList.add("beer_label");
     kegIMG.style.width = "60%";
     kegContainer.appendChild(kegIMG);
@@ -74,9 +74,36 @@ export function kegsStart() {
     let kegStorage = document.createElement("div");
     kegStorage.textContent = storage;
     kegStorage.classList.add("storage");
+    kegStorage.id = "storage" + number;
     kegContainer.appendChild(kegStorage);
 
-    document.querySelector(`#keg${number} #Rectangle_174`).style.height = "120";
-    document.querySelector(`#keg${number} #Rectangle_174`).style.y = "50";
+    if (level > 2000) {
+      document.querySelector(`#keg${number} #Rectangle_174`).style.height =
+        "120";
+      document.querySelector(`#keg${number} #Rectangle_174`).style.y = "50";
+    }
+
+    if (level < 2000 && level > 1000) {
+      document.querySelector(`#keg${number} #Rectangle_174`).style.y = "150";
+      document.querySelector(`#keg${number} #Rectangle_174`).style.height =
+        "70";
+      document.querySelector(`#keg${number} #Rectangle_173`).style.height =
+        "100";
+      document.querySelector(`#keg${number} #Rectangle_173`).style.y = "170";
+    }
+
+    if (level < 1000 && level > 200) {
+      document.querySelector(`#keg${number} #Rectangle_174`).style.y = "180";
+      document.querySelector(`#keg${number} #Rectangle_174`).style.height =
+        "40";
+      document.querySelector(`#keg${number} #Rectangle_173`).style.height =
+        "90";
+      document.querySelector(`#keg${number} #Rectangle_173`).style.y = "180";
+    }
+
+    if (level < 200) {
+      document.querySelector(`#keg${number} #Rectangle_174`).style.height = "0";
+      document.querySelector(`#keg${number} #Rectangle_173`).style.height = "0";
+    }
   }
 }
